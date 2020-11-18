0b3bd5a30ec53d54e9952f65bff8c054854ae8cc
Sergey Karbainov <karbainovs@mail.ru>
2020-11-18 20:42:35 +0800

    geehrc: Hello Lineage OS

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_WIDTH := 768
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/nitrogen/products/common.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/geehrc/full_geehrc.mk)
